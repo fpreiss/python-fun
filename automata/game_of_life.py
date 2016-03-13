183ec4eba269c23285171afc948a622b16cd613a
fpreiss <fa.preiss@web.de>
2016-03-13 17:01:50 +0100

    Update game_of_life.py

--- automata/game_of_life.py
+++ automata/game_of_life.py
@@ -6,7 +6,7 @@ m0,kernel,fig,im=((random.randint(0,100,[256,256])<30)*1,ones([3,3]),pyplot.figu
 im.append((pyplot.imshow(m0, cmap="gray", interpolation="none"),))
 for i in range(0,1000):
     conv=sg.convolve2d(m0,kernel,mode='same',boundary='wrap')
-    m0=(m0*((conv>2) * (conv<5)))+(1-m0)*1*(conv==3)
+    m0=(m0*((conv>2) * (conv<5)))+(1-m0)*(conv==3)
     im.append((pyplot.imshow(m0, cmap="gray", interpolation="none"),))
 im_ani = animation.ArtistAnimation(fig, im, interval=50, repeat_delay=0, blit=True)
 pyplot.show()
